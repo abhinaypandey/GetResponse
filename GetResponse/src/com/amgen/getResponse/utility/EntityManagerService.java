f4b23ce9b7a8503005fe09383c53d881e2b68e06
abhinaypandey <abhinaya18@gmail.com>
2015-01-16 16:43:36 +0530

    Version 1.2

--- GetResponse/src/com/amgen/getResponse/utility/EntityManagerService.java
+++ GetResponse/src/com/amgen/getResponse/utility/EntityManagerService.java
@@ -12,7 +12,6 @@ import java.util.logging.SimpleFormatter;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import org.apache.log4j.Logger;
 
 public class EntityManagerService {
